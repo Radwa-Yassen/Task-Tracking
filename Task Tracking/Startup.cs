1755320ce3e8745e8ef6f667691cfe6293379289
radwa.yassen99 <radwa.yassen99@hotmail.com>
2017-08-07 20:02:14 +0200

    creating porject

---
+++ Task Tracking/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Task_Tracking.Startup))]
+namespace Task_Tracking
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
